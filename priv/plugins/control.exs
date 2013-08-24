8e8b781c71481130c02c38872f0b679cee5b8a77
meh <meh@schizofreni.co>
2013-08-24 17:25:44 +0200

    plugin/control: remove test code

--- priv/plugins/control.exs
+++ priv/plugins/control.exs
@@ -4,10 +4,4 @@ defplugin :control do
   def init(_options) do
     { :ok, State[] }
   end
-
-  def handle(Event.Message[content: "I like trains"] = msg, _state) do
-    msg.reply "me too! :DDDDDDDD"
-
-    { :ok, _state }
-  end
 end
